8336fdad50679c4fa50277a7856101c82461879e
정재훈 <test@test>
2023-02-17 17:07:08 +0900

    test

---
+++ src/main/java/jh/test/JenkinsTestJarApplication.java
@@ -0,0 +1,13 @@
+package jh.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestJarApplication.class, args);
+	}
+
+}
